bb3b3d00b4ee60c6c828791f9ad694ce6778c510
JerryEssandoh <69096311+JerryEssandoh@users.noreply.github.com>
2024-07-23 18:50:06 -0400

    Update launch_record.sh

--- scripts/launch/launch_record.sh
+++ scripts/launch/launch_record.sh
@@ -2,4 +2,4 @@
 # For some reason the rs-integrate service will ONLY work if launched as an entry point!
 # https://github.com/continental/ecal/issues/238
 timestamp=`date "+%Y%m%d-%H%M%S"` #add %3N as we want millisecond too
-ecal_rec_client --whitelist "MeshAndTouchdownMessage,PoseMessage,RGBDLandingMessage,RGBDMessage,TouchdownMessage,LandingMessage" -d /home/jeremy/ecal_meas -n "$timestamp" -r
+ecal_rec_client --whitelist "MeshAndTouchdownMessage,PoseMessage,RGBDLandingMessage,RGBDMessage,TouchdownMessage,LandingMessage" -d /home/a2sys/ecalmeas -n "$timestamp" -r
